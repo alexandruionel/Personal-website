95e283f3fe069b96b57c2eb9faf4c72ae49c6edb
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2021-11-18 11:09:24 +0200

    added updates to Hero section, and some responsive adjustments.

--- js/main.js
+++ js/main.js
@@ -37,16 +37,16 @@ Menu
 			closebtn.addEventListener( 'click', toggleMenu );
 		}
 
-		/* close the menu element if the target it´s not the menu element or one of its descendants..
-		content.addEventListener( 'click', function(ev) {
-			var target = ev.target;
-			if( isOpen && target !== openbtn ) {
-				toggleMenu();
-			}
-		} );
-		*/
+		//  close the menu element if the target it´s not the menu element or one of its descendants..
+		// content.addEventListener( 'click', function(ev) {
+		// 	var target = ev.target;
+		// 	if( isOpen && target !== openbtn ) {
+		// 		toggleMenu();
+		// 	}
+		// } );
+		
 	}
-
+	
 	function toggleMenu() {
 		if( isOpen ) {
 			classie.remove( bodyEl, 'show-menu' );
